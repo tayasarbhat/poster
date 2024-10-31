a7be7fb9697e1a8bcc4dca7cd08d4ac781be29ba
tayasarbhat <tayasarbhat@gmail.com>
2024-10-31 21:36:40 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import path from 'path';
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/poster/', // Replace 'your-repo-name' with the actual repository name
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
 });
